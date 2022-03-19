de291f3da589e20cd8fc9d416bba7e446279273a
Filip Horvat <filip.horvat@live.com>
2022-03-19 13:14:24 +0100

    Another save

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import "./App.css";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Slider from "@mui/material/Slider";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">React Playground</header>
+      <h1>React Playground</h1>
+      <div className="ButtonList">
+        <p>Background Color</p>
+        <Button>Red</Button>
+        <Button>Green</Button>
+        <Button>Blue</Button>
+      </div>
+      <div className="Slider">
+        <p>Opacity</p>
+        <Slider></Slider>
+      </div>
     </div>
   );
 }
